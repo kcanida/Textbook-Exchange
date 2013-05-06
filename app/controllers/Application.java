b25c94a75e9788d166a7c3843bd91d46cb6db98d
Kellie Canida <kelliecanida@hotmail.com>
2013-05-06 10:50:46 -1000

    added login method to render login page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,19 @@
 package controllers;
 
+import java.util.List;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
   
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+  public static Result index() {
+    return ok(index.render());
+  }
+  
+  public static Result login() {
+    return ok(login.render());
+  }
   
 }
+
